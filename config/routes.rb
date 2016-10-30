f7a9663bbbb875c05af8918257c2b397a735fcf1
dimanyc <dimitrynazarov@gmail.com>
2016-10-30 19:33:07 -0400

    inits webpack config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'hello_world', to: 'hello_world#index'
   root to: 'static_pages#home'
   match 'auth/twitter/callback', to: 'sessions#create', via: [:get, :post]
   get '/dashboard', to: 'dashboard#show'
