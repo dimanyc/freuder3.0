53d8b2d95cc820b8a96f514edc37ad8c5d760c80
dimanyc <dimitrynazarov@gmail.com>
2016-12-21 20:20:54 -0500

    adds spec for unauth access to Dashboard#show

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -4,15 +4,35 @@ RSpec.describe DashboardController, type: :controller do
 
   context '#show' do
 
-    before do
-      stub_twitter_api
-      user = create(:user)
-      sign_in(user)
-      get :show
+    context 'with valid session data' do
+
+      before do
+        stub_twitter_api
+        user = create(:user)
+        sign_in(user)
+        get :show
+      end
+
+      it 'returns 200 response status' do
+        expect(response.status).to eq(200)
+      end
+
     end
 
-    it 'returns 200 response status' do
-      expect(response.status).to eq(200)
+    context 'with invalid session data' do
+
+      before(:each) do
+        get :show
+      end
+
+      it 'responds with 304' do
+        expect(response.status).to eq(302)
+      end
+
+      it 'redirects visitors to root path' do
+        expect(response).to redirect_to root_path
+      end
+
     end
 
   end
